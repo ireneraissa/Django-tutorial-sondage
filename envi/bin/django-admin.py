2b934a9bc1c0b09d7274be72613fcb872744ceb3
irene <i.kameni@fireflymedia.tv>
2017-06-15 11:47:58 +0000

    My Django-tuto app,firs commit

---
+++ envi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/irene/Documents/stage/programme/django-tutorial/envi/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
